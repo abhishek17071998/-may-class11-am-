7ed653250db0ba8ea702032e75b156cfee50713f
Meenu <Meenu@LAPTOP-TOE98MBN>
2022-07-13 19:00:32 +0530

    1st time testing

---
+++ automation/src/automation/testing.java
@@ -0,0 +1,19 @@
+package automation;
+
+public class testing 
+{
+	public void m1()
+	{
+		int a=10;
+		int b=20;
+		int c;
+		c=a+b;
+		System.out.println("sum of the number " +c);
+		
+	}
+public static void main(String[] args) 
+{
+	testing obj=new testing();
+	obj.m1();
+}
+}
